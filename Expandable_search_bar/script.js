2eac339c1a2d780ddca56c9dafe1cbfa17ce8ede
CremonaLambert <cremonalambertmw@gmail.com>
2023-01-30 02:55:33 +0200

    codestore added

--- Expandable_search_bar/script.js
+++ Expandable_search_bar/script.js
@@ -1,10 +1,18 @@
 
 const icon = document.querySelector(".icon");
 const searchContainer = document.querySelector(".search_container");
-icon.onclick = function(){
-    searchContainer.classList.toggle("active")
-}
+// icon.onclick = function(){
+//     searchContainer.classList.toggle("active")
+// }
+// icon.addEventListener('click', function(e){
+//    searchContainer.classList.toggle('active');
+// })
 
+if(icon){
+    addEventListener('click', function(e){
+        searchContainer.classList.toggle('active')
+    })
+}
 
 const typeYourText = document.getElementById('wesearch');
 const clear = document.querySelector('.clear');
