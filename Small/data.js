f5ff6f50898b4e2f2468bd9ce7a27aaef86ace6e
CremonaLambert <cremonalambertmw@gmail.com>
2023-02-18 14:18:35 +0200

    importation of firstposts

---
+++ Small/data.js
@@ -0,0 +1,42 @@
+export const firstPosts = [
+    {
+        uuid: 1,
+        comments: 23,
+        faculty: "engineering",
+        university: "Ben Kas University",
+        likes: 1132,
+        text: "Would never go back"
+    },
+    {
+        uuid: 2,
+        comments: 83,
+        faculty: "economics",
+        university: "Money University",
+        likes: 972,
+        text: "I'm recommending this to anyone"
+    },
+    {
+        uuid: 3,
+        comments: 186,
+        faculty: "engineering",
+        university: "Random University",
+        likes: 620,
+        text: "The best experience"
+    },
+    {
+        uuid: 4,
+        comments: 88,
+        faculty: "architecture",
+        university: "Africa University",
+        likes: 2551,
+        text: "Something to never go through again"
+    },
+    {
+        uuid: 5,
+        comments: 8,
+        faculty: "engineering",
+        university: "Ben Kas University",
+        likes: 23,
+        text:"Well, it is what it is"
+    }
+];
--- Small/main.js
+++ Small/main.js
@@ -1,4 +1,11 @@
-function cremona (){
-    alert ("cremona Lambert");
-    
+import { firstPosts } from "./data.js";
+
+function cremona (num1, num2, num3){
+    let sum = num1 + num2 + num3;
+    return sum; 
 }
+
+result = cremona(12, 45, 6);
+console.log(result);
+
+// topNposts(7, "likes", POSTS)  // should return the top 7 univs if we have at least 7 posts, else return the maximum univs available
